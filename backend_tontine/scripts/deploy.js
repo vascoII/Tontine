b58095a0bb3cb0b8f9ebde6a3b2b36baf69e1d31
VascoII <fabriceds@yahoo.fr>
2024-02-14 18:29:14 +0100

    Update: simplify script for deploy on sepolia testnet

--- backend_tontine/scripts/deploy.js
+++ backend_tontine/scripts/deploy.js
@@ -54,27 +54,6 @@ async function main() {
 
   console.log("Tine contract deployed to:", tineSmartContractAddress);
 
-  // Envoi de 50 ethers au contrat
-  const transaction = {
-    to: tineSmartContractAddress,
-    value: hre.ethers.parseEther("50.0"), // Convertit 50 ethers en wei
-  };
-
-  const tx = await deployer.sendTransaction(transaction); // Envoie la transaction
-  await tx.wait(); // Attend que la transaction soit minée
-
-  console.log(`50 ethers send to ${tineSmartContractAddress}`);
-
-  // Lock des Tine du owner pour par la suite lancer le vault Gold
-  // Récupérer l'instance du contrat Tine
-  const tineInstance = await ethers.getContractAt(
-    "Tine",
-    tineSmartContractAddress
-  );
-  // Verrouiller les TINE
-  await tineInstance.lockTine();
-  console.log(`Lock Tine's owner on ${tineSmartContractAddress}`);
-
   /*******************************************************/
   /*********************  TONTINE  ***********************/
   /*******************************************************/
@@ -87,28 +66,6 @@ async function main() {
   const tontineSmartContractAddress = await tontine.getAddress();
 
   console.log("Tontine contract deployed to:", tontineSmartContractAddress);
-
-  // Ajout de 10 ETH en staking sur un node RocketPool de Tontine par silver
-  const tontineInstance = await ethers.getContractAt(
-    "Tontine",
-    tontineSmartContractAddress
-  );
-  const silverVaultInitialBalance = hre.ethers.parseEther("10.0");
-  await tontineInstance.depositEth(false, {
-    value: silverVaultInitialBalance,
-  });
-  console.log(
-    "The Tontine's Silver Vault has been launched and funded with 10 ETH."
-  );
-
-  // Ajout de 32 ETH en staking sur un node RocketPool de Tontine par gold
-  const goldVaultInitialBalance = hre.ethers.parseEther("32.0");
-  await tontineInstance.depositEth(true, {
-    value: goldVaultInitialBalance,
-  });
-  console.log(
-    "The Tontine's Gold Vault has been launched and funded with 32 ETH."
-  );
 }
 
 main().catch((error) => {
