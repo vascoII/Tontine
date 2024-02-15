0564e11e36b6fa81f2c967c3a031eddfb22485f4
VascoII <fabriceds@yahoo.fr>
2024-02-15 02:44:32 +0100

    Updated: deploy fil for backend interaction with contracts

--- backend_tontine/scripts/deploy.js
+++ backend_tontine/scripts/deploy.js
@@ -66,6 +66,50 @@ async function main() {
   const tontineSmartContractAddress = await tontine.getAddress();
 
   console.log("Tontine contract deployed to:", tontineSmartContractAddress);
+
+  /*******************************************************/
+  /***************  INTERACTION BACKEND  *****************/
+  /*******************************************************/
+
+  /**********************  TINE  *************************/
+  const tineInstance = await ethers.getContractAt(
+    "Tine",
+    tineSmartContractAddress
+  );
+  // Achat de 1 Tine sur Tine smart contract
+  const ethAmountToBuyTine = hre.ethers.parseEther("1.0");
+  const tineAmountToBuy = hre.ethers.parseEther("1.0");
+  await tineInstance.buyTine(tineAmountToBuy, {
+    value: ethAmountToBuyTine,
+  });
+  console.log("We bought 1 Tine as first step to access the gold vault.");
+
+  // Verrouiller le TINE
+  await tineInstance.lockTine();
+  console.log(`Lock Tine's owner on ${tineSmartContractAddress}`);
+
+  /*********************  TONTINE  ***********************/
+  const tontineInstance = await ethers.getContractAt(
+    "Tontine",
+    tontineSmartContractAddress
+  );
+  // Ajout de 10 ETH en staking sur un RocketPool par silver
+  const silverVaultInitialBalance = hre.ethers.parseEther("10.0");
+  await tontineInstance.depositEth(false, {
+    value: silverVaultInitialBalance,
+  });
+  console.log(
+    "The Tontine's Silver Vault has been launched and funded with 10 ETH."
+  );
+
+  // Ajout de 32 ETH en staking sur un node RocketPool de Tontine par gold
+  const goldVaultInitialBalance = hre.ethers.parseEther("32.0");
+  await tontineInstance.depositEth(true, {
+    value: goldVaultInitialBalance,
+  });
+  console.log(
+    "The Tontine's Gold Vault has been launched and funded with 32 ETH."
+  );
 }
 
 main().catch((error) => {
