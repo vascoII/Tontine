93589ac9372c4cb1e2598c72ce9c67f171f7750e
VascoII <fabriceds@yahoo.fr>
2024-02-15 02:43:37 +0100

    Updated: abi and contractAddress

--- frontend_tontine/constants/index.js
+++ frontend_tontine/constants/index.js
@@ -727,7 +727,7 @@ export const abiTine = [
 ];
 
 export const contractAddressTontine =
-  "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
+  "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
 export const abiTontine = [
   {
     inputs: [
