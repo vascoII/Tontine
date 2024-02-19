7a767d1ab106d02f5ca3084090ee37535c3de2ac
VascoII <fabriceds@yahoo.fr>
2024-02-19 13:55:10 +0100

    Added: polygon network

--- backend_tontine/hardhat.config.js
+++ backend_tontine/hardhat.config.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_KEY;
 
+const ALCHEMY_POLYGON_API_KEY = process.env.ALCHEMY_POLYGON_API_KEY;
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.22",
@@ -14,6 +17,10 @@ module.exports = {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_POLYGON_API_KEY}`,
+      accounts: [MUMBAI_PRIVATE_KEY],
     },
   },
 };
